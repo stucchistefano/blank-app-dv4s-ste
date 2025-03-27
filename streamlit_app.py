fd774806157ca5999e55c7ba84ea74d93648e72a
stucchistefano <stefano4.stucchi@mail.polimi.it>
2025-03-27 09:28:49 +0000

    I did some changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+# We need to import the streamlit library as first
 
+# Title
 st.title("🎈 My new app")
+
+# Text and writer
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+# Data
+value = st.slider("Select a specific value: ", 0, 100, 30)
+# The values are in order the minimum, the maximum and the actual value of the slider
+
+# To write the value of the slider (at screen)
+st.write("You selected: ", value)
